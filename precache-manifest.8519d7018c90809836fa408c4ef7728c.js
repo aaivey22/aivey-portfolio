3ff3aa048b9eee4e06f5598459a25d36f09d3f4c
aaivey22 <angelawings22@gmail.com>
2020-07-28 12:39:17 -0400

    Updates

---
+++ precache-manifest.8519d7018c90809836fa408c4ef7728c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e7f75935fa93b9208522bc5300db10",
+    "url": "/aivey-portfolio/index.html"
+  },
+  {
+    "revision": "ac5b72339f5d98adff1e",
+    "url": "/aivey-portfolio/static/css/main.26472ffa.chunk.css"
+  },
+  {
+    "revision": "7bafcad3adabdcb8e0af",
+    "url": "/aivey-portfolio/static/js/2.385154c0.chunk.js"
+  },
+  {
+    "revision": "6c48804b32a0960f7f0c75e8534a0365",
+    "url": "/aivey-portfolio/static/js/2.385154c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5b72339f5d98adff1e",
+    "url": "/aivey-portfolio/static/js/main.a729d1cf.chunk.js"
+  },
+  {
+    "revision": "6ffa6d210d3464e8789e",
+    "url": "/aivey-portfolio/static/js/runtime-main.e8202589.js"
+  },
+  {
+    "revision": "5b16128d9460676ce7669379d73c6c49",
+    "url": "/aivey-portfolio/static/media/avatar.5b16128d.png"
+  },
+  {
+    "revision": "31e9ad7bdaf88d98126956c4c75e1d39",
+    "url": "/aivey-portfolio/static/media/burger-meister-gif.31e9ad7b.gif"
+  },
+  {
+    "revision": "a76b055e1125c0308da151ce2a6de66b",
+    "url": "/aivey-portfolio/static/media/inspirit.a76b055e.JPG"
+  },
+  {
+    "revision": "3e45985e13d682d3f4df0d821966bf0d",
+    "url": "/aivey-portfolio/static/media/jotter.3e45985e.gif"
+  },
+  {
+    "revision": "160feb50b62b0d63a5e30fc8e0282bad",
+    "url": "/aivey-portfolio/static/media/myFamiliar.160feb50.png"
+  },
+  {
+    "revision": "98cb1393970fb9fff4336985d900e75e",
+    "url": "/aivey-portfolio/static/media/overYonder.98cb1393.gif"
+  },
+  {
+    "revision": "06cb3ad0956159bb210213037a95fbfe",
+    "url": "/aivey-portfolio/static/media/read.06cb3ad0.gif"
+  },
+  {
+    "revision": "ac172f94d6d1d166f3c72250e68bdcac",
+    "url": "/aivey-portfolio/static/media/rockymtn.ac172f94.jpg"
+  }
+]);
